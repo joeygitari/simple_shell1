bf8788ebd22838a5e016942c912f9ff1bd9f30e1
joeygitari <gitarijoanne@gmail.com>
2023-02-11 00:24:52 -0800

    second commit

---
+++ fork.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *  * _fork - calls fork function and checks its success
+ *   * @cmd: command to execute
+ *    * @av: command's argument
+ *     *
+ *      * Return: process id
+ *       */
+int _fork(char *cmd, char **av)
+{
+	int id, status;
+
+
+	id = fork();
+	if (id == -1)
+		err("Error: Unsucessful fork\n");
+	if (id == 0)
+	{
+		if (execve(cmd, av, environ) == -1)
+			err("Err: Could not execute command\n");
+	}
+	else
+		wait(&status);
+	return (id);
+}
